d9dacb84bfe99356b853afa585b94e9822b39444
tbalint19 <tothbalint719@gmail.com>
2024-05-06 18:06:25 +0200

    random pk

--- backend/src/database/schema.ts
+++ backend/src/database/schema.ts
@@ -1,14 +1,14 @@
-import { integer, pgTable, serial, varchar } from "drizzle-orm/pg-core"
+import { integer, pgTable, text, uuid } from "drizzle-orm/pg-core"
 
 export const profiles = pgTable("profiles", {
-  id: serial("id").primaryKey(),
-  googleId: varchar("google_id", { length: 255 }).notNull(),
-  username: varchar("email", { length: 255 }).notNull(),
+  id: uuid("id").primaryKey().defaultRandom(),
+  googleId: text("google_id").notNull(),
+  username: text("email").notNull(),
 })
 
 export const cars = pgTable("cars", {
-  id: serial("id").primaryKey(),
-  brand: varchar("brand", { length: 255 }).notNull(),
+  id: uuid("id").primaryKey(),
+  brand: text("brand").notNull(),
   topSpeed: integer("top_speed").notNull(),
-  ownerId: integer("owner_id").references(() => profiles.id),
+  ownerId: uuid("owner_id").references(() => profiles.id),
 })
